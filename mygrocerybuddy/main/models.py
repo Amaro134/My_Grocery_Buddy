bb8764a4ae5f962d8624c51c9754b8189172397a
Amaro134 <amarogracie134@gmail.com>
2025-12-02 09:13:08 +0300

    worked on the validation

--- mygrocerybuddy/main/models.py
+++ mygrocerybuddy/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to='profile_pics/', default='default.png')
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+# Automatically create or update Profile when User is created/updated
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
